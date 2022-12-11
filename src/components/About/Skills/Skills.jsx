c4bee515300e9b7e582f7907bdbb90b39d6100e8
BADTRY <95194385+BADTRY@users.noreply.github.com>
2022-12-11 17:23:50 +0800

    Deployed website

--- src/components/About/Skills/Skills.jsx
+++ src/components/About/Skills/Skills.jsx
@@ -56,7 +56,7 @@ function Skills() {
                     <p> •Knowledge using Microsoft tools like Word, Powerpoint and others</p>
                     <p> •Basic knowledge in editing and designing software like Photoshop, Figma and Filmora</p>
                 </div>
-                <img ref={ref} src="./Assets/Skill.PNG" alt="skillss" />
+                <img ref={ref} src="./Assets/Skill.PNG" alt="Skills" />
             </motion.div>
             
         </div>
